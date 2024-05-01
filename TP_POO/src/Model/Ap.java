a140b37c1adfb00b7df700c6d2550b38290f7104
Nassim <nassimhessas60@gmail.com>
2024-05-01 17:34:29 +0100

    Mongo connection et inserer document

---
+++ TP_POO/src/Model/Ap.java
@@ -0,0 +1,33 @@
+package Mongo;
+
+import Code.*;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.*;
+
+public class MongoFunctions {
+    public static MongoCollection<Document> mongconnect(String dbName, String collName) {
+        String uri = "mongodb+srv://nassimhessas60:nassimhessas60@cluster0.3ye673x.mongodb.net/";
+        MongoClientURI clientURI = new MongoClientURI(uri);
+        MongoClient mongoClient = new MongoClient(clientURI);
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(dbName);
+        System.out.println("DataBase connected");
+        MongoCollection collection = mongoDatabase.getCollection(collName);
+        return collection;
+    }
+
+    public static void insertDoc(MongoCollection collection, Document document) {
+        collection.insertOne(document);
+        System.out.println("Document inserted successfully!");
+    }
+}

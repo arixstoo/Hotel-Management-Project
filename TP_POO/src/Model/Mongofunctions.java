ae7974b41031fd9081faba89db2913134f0a470c
Nassim <nassimhessas60@gmail.com>
2024-05-02 16:07:38 +0100

    + modification + operations sur les clients

---
+++ TP_POO/src/Model/Mongofunctions.java
@@ -0,0 +1,14 @@
+import Code.*;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.*;

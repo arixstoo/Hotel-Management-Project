4fea6462760dd6dd7bd31463abfb5b04f2fb127c
nassimHes <147937602+nassimHes@users.noreply.github.com>
2024-05-25 12:44:35 +0100

    Update ChambreOper.java

--- TP_POO/src/Model/ChambreOper.java
+++ TP_POO/src/Model/ChambreOper.java
@@ -1,24 +1,14 @@
 package Model;
 
-import Controller.*;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
-import org.bson.types.ObjectId;
-
-import java.util.*;
 
 public class ChambreOper {
 
 
-    /* inserer une chambre */ public static void insertChambre(int numChambre, String type, int prix) {
+    /* inserer une chambre */
+    public static void insertChambre(int numChambre, String type, int prix) {
         MongoCollection collection =MongoFunctions.mongconnect("TP_Hotel","Chambres");
 
         Document document = new Document("NumChambre", numChambre)
@@ -29,8 +19,10 @@ public class ChambreOper {
         collection.insertOne(document);
         System.out.println("Chambre crée avec succés");
     }
-    /* supprimer une chambre */ public static void suppChambre(Object value) {
-        MongoCollection collection = MongoFunctions.mongconnect("TP_Hotel","Chamres");
+
+    /* supprimer une chambre */
+    public static void suppChambre(Object value) {
+        MongoCollection collection = MongoFunctions.mongconnect("TP_Hotel","Chambres");
         Document search = new Document();
         search.put("NumChambre", value);
 
@@ -38,7 +30,7 @@ public class ChambreOper {
         long deletedCmp = deleteResult.getDeletedCount();
 
         if (deletedCmp > 0) {
-            if (deletedCount == 1) {
+            if (deletedCmp == 1) {
                 System.out.println(deletedCmp + " Chambre supprimée avec succès ");
             } else {
                 System.out.println(deletedCmp + " Chambres supprimées avec succès ");

023cc6049bae6dd7b846932de4521aa1a45c0f65
Asma AB <114869927+Whereisthelichi@users.noreply.github.com>
2024-05-25 12:54:19 +0100

    Update Client_view.java

--- TP_POO/src/Vue/Client_view.java
+++ TP_POO/src/Vue/Client_view.java
@@ -1,13 +1,35 @@
 package Vue;
 
+import Controller.*;
+import Controller.Exceptions.Exception_text;
+import Model.MongoFunctions;
+import com.mongodb.client.MongoCollection;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
-class ClientView extends JFrame {
-    public ClientView() {
+public class Client_view extends JFrame {
+
+    // Variable pour suivre l'état de l'annulation de la réservation
+    public static final boolean[] reservationAnnulee = {false};
+    public static final boolean[] reservationModifiee = {false};
+
+    public static ArrayList<Integer> chambres = new ArrayList<>();
+    public static Reservation res = new Reservation();
+
+    public Client_view(String usernameClient) {
+        Main.charger();
         // Set up the frame
-        setTitle("Client View");
+        setTitle("Bienvenue à notre hôtel !");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Image img = tk.getImage("src/Vue/IconHotel.png");
+        setIconImage(img);
         setSize(650, 450); // Adjusted frame size
         setLocationRelativeTo(null);
 
@@ -28,14 +50,12 @@ class ClientView extends JFrame {
             }
         };
 
-
-
         mainPanel.setOpaque(false);
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
 
-   //here i creat the background panel
-        ImageIcon backgroundImage = new ImageIcon("image8.jpg");
+        //here i creat the background panel
+        ImageIcon backgroundImage = new ImageIcon("src/Vue/image8.jpg");
         JLabel backgroundLabel = new JLabel(backgroundImage);
         backgroundLabel.setLayout(new GridBagLayout());
 
@@ -49,42 +69,95 @@ class ClientView extends JFrame {
         // Space between buttons
         buttonsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
-         // Button 1
-        JButton button1 = new JButton("     Faire une Reservation    ");
+        // Button 1
+        JButton button1 = new JButton("       Faire une réservation      ");
         button1.setBackground(new Color(100, 100, 100)); // Dark grey background
         button1.setForeground(Color.WHITE);
+        button1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                try{
+                    //vérifier si cet utilisateur a déjà une réservation
+                    Boolean trouve = false;
+                    if(Main.reservations.containsKey(usernameClient)){
+                        trouve=true;
+                    }
+                    if(trouve==true){
+                        throw new Exception_text("Vous avez fait une reservation déjà, vous ne pouvez pas faire une autre pour le moment juste la modifier.");
+                    } else{
+                        dispose();
+                        reservation secondWindow = new reservation(usernameClient,0);
+                        secondWindow.setVisible(true);
+                    }
+                } catch(Exception_text ex){
+                    JOptionPane.showMessageDialog(null, ex.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
         button1.setPreferredSize(new Dimension(250, 60)); // Adjust button size
         buttonsPanel.add(button1);
 
         buttonsPanel.add(Box.createRigidArea(new Dimension(0, 30)));
 
         // Button 2 (Edit Reservation)
-        JButton button2 = new JButton("   Modifier ta Reservation   ");
+        JButton button2 = new JButton("     Modifier la réservation       ");
         button2.setBackground(new Color(100, 100, 100)); // Dark grey background
         button2.setForeground(Color.WHITE);
+        button2.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                try {
+                    // Vérifiez si cet utilisateur a déjà annulé la réservation
+                    if (Client_view.reservationModifiee[0]) {
+                        JOptionPane.showMessageDialog(null,"Vous avez déjà modifié votre réservation.","Erreur",JOptionPane.ERROR_MESSAGE);
+                        return; // Sortez de la méthode car l'annulation a déjà été faite
+                    }
+
+                    //vérifier si cet utilisateur a déjà une réservation
+                    Boolean trouvé = false;
+                    if(Main.reservations.containsKey(usernameClient)){
+                        trouvé=true;
+                    }
+
+                    if(trouvé) {
+                        Client.modifierRéservation(usernameClient);
+                    } else{
+                        throw new Exception_text("Vous n'avez fait aucune reservation, vous ne pouvez rien modifier pour le moment.");
+                    }
+                } catch (Exception_text ex) {
+                    JOptionPane.showMessageDialog(null,ex.getMessage(),"Erreur",JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
         button2.setPreferredSize(new Dimension(250, 60)); // Adjust button size
         buttonsPanel.add(button2);
 
         buttonsPanel.add(Box.createRigidArea(new Dimension(0, 30)));
 
         // Button 3 (Check-in / Check-out)
-        JButton button3 = new JButton("    Effacer ta Reservation    ");
+        JButton button3 = new JButton("       Annuler la réservation      ");
         button3.setBackground(new Color(100, 100, 100)); // Dark grey background
         button3.setForeground(Color.WHITE);
+        button3.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                Client.supprimerRéservation(usernameClient);
+                Main.charger();
+            }
+        });
         button3.setPreferredSize(new Dimension(250, 60)); // Adjust button size
         buttonsPanel.add(button3);
+
         mainPanel.add(buttonsPanel);
 
         backgroundLabel.add(mainPanel, gbc);
         add(backgroundLabel, BorderLayout.CENTER);
     }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new ClientView().setVisible(true);
-            }
-        });
+    public static void garderInfos(String usernameClient){
+        for(int j=0;j<Main.reservations.get(usernameClient).getChambre().size();j++){
+            Client_view.chambres.add(Main.reservations.get(usernameClient).getChambre().get(j).numéroChambre);
+        }
+        Client_view.res = Main.reservations.get(usernameClient);
     }
 }
 
